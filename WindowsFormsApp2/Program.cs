7fbd41a6012ad504a4b9fd8a3b4e504d8b731430
RudyMeijer <RudyMeijer@gmail.com>
2017-09-30 16:08:17 +0200

    Add test1

--- WindowsFormsApp2/Program.cs
+++ WindowsFormsApp2/Program.cs
@@ -14,9 +14,15 @@ namespace WindowsFormsApp2
 		[STAThread]
 		static void Main()
 		{
+			Test1();
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Form1());
+		}
+
+		private static void Test1()
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
