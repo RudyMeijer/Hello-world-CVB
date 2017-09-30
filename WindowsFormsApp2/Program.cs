4bb8ef0e377a1085c5c6fa68a1be59d05f519a2d
RudyMeijer <RudyMeijer@gmail.com>
2017-09-30 16:16:48 +0200

    Add test 2 in code analisis branch only

--- WindowsFormsApp2/Program.cs
+++ WindowsFormsApp2/Program.cs
@@ -17,6 +17,12 @@ namespace WindowsFormsApp2
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Form1());
+			Test2();
+		}
+
+		private static void Test2()
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
